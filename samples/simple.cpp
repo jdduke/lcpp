f638c7feae1933f9b7487638f0b4a7fc9e3fe75b
Jared D <glorindel@gmail.com>
2012-04-03 19:22:14 -0700

    Using iota for sequence generation.

--- samples/simple.cpp
+++ samples/simple.cpp
@@ -13,29 +13,30 @@
 int main(int argc, char* argv[]) {
 
   using namespace lc;
+  using namespace std;
 
   ///////////////////////////////////////////////////////////////////////////
   // arrays, vectors and other templated containers are all supported
 
-  const std::array<int, 10> a = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  const std::array<int, 10> b = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  const array<int, 10> a = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  const array<int, 10> b = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
 
   ///////////////////////////////////////////////////////////////////////////
   // Multiple sources, single condition
 
-  std::cout << "x < y: " << std::endl
+  cout << "x < y: " << endl
             << (from(a, b) , [](int x, int y) { return x < y; })()
-            << std::endl << std::endl;
+            << endl << endl;
 
 
   ///////////////////////////////////////////////////////////////////////////
   // Multiple sources, multiple conditions
 
-  std::cout << "(x + y < z) && (x > y): " << std::endl
+  cout << "(x + y < z) && (x > y): " << endl
             << (from(a, a, a) , [](int x, int y, int z) { return x + y < z; }
                               , [](int x, int y, int z) { return x > y; })()
-            << std::endl<< std::endl;
+            << endl<< endl;
 
 
   ///////////////////////////////////////////////////////////////////////////
@@ -43,21 +44,21 @@ int main(int argc, char* argv[]) {
 
   auto list = (from(a, b), [](int x, int y) { return x == y; });
 
-  std::cout << "(x == y) using iterators: " << std::endl;
-  for(auto it = std::begin(list); it != std::end(list); ++it) {
-    std::cout << *it;
+  cout << "(x == y) using iterators: " << endl;
+  for(auto it = list.begin(); it != list.end(); ++it) {
+    cout << *it;
   }
 
-  std::cout << std::endl << std::endl;
+  cout << endl << endl;
 
 
 ///////////////////////////////////////////////////////////////////////////
   // Multiple sources, multiple conditions and a selecting transform
 
   auto mult_triple = [](int x, int y, int z) { return x * y * z; };
-  std::cout << "(x*y*z) where (x+y+z==10):" << std::endl
+  cout << "(x*y*z) where (x+y+z==10):" << endl
             << (mult_triple | (from(a, a, a), [](int x, int y, int z) { return x + y + z == 10; }))()
-            << std::endl << std::endl;
+            << endl << endl;
 
 
   ///////////////////////////////////////////////////////////////////////////
@@ -65,20 +66,16 @@ int main(int argc, char* argv[]) {
 
   auto tf   = [](int x, int y) { return x + y;  };
   auto cond = [](int x, int y) { return x == y; };
-  std::cout << "Untransformed: " << (from(a, b), cond)() << std::endl;
-  std::cout << "Transformed:   " << (tf | (from(a, b), cond))() << std::endl;
+  cout << "Untransformed: " << (from(a, b), cond)() << endl;
+  cout << "Transformed:   " << (tf | (from(a, b), cond))() << endl;
 
   ///////////////////////////////////////////////////////////////////////////
   // Pythagorean triples
 
-  auto range = [](int i0, int n) -> std::vector<int> {
-    std::vector<int> r;
-    std::generate_n(std::back_inserter(r), n, [=]() mutable { return i0++; });
-    return r;
-  };
-  auto r = range(1, 20);
-  std::cout << "PythagoreanTriples: "
+  vector<int> r(20);
+  iota(std::begin(r), std::end(r), 1);
+  cout << "PythagoreanTriples: "
             << (from(r, r, r), [](int x, int y, int z) { return x < y && y < z; },
-                               [](int x, int y, int z) { return x*x + y*y == z*z; })() << std::endl;
+                               [](int x, int y, int z) { return x*x + y*y == z*z; })() << endl;
 
 }
